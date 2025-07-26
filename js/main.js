9c8fb3edb4193e90100d6d8152fd7d0700fb22d1
habiba-tech <siddiquiummehabiba41@gmail>
2025-07-27 00:27:11 +0530

    initial Commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Headrr scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 50 ){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.add("header-scrolled");
+    }
+}
+
+//nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(e){
+    e.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
